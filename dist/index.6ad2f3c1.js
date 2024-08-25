2f3b7b98aae52e8e96b5e794d2e1ad1485b999fa
Sanna Olsson <saol1901@student.miun.se>
2024-08-25 09:48:29 +0200

    dist

---
+++ dist/index.6ad2f3c1.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",()=>{let e=document.getElementById("hamburger"),t=document.getElementById("navbar");e.addEventListener("click",()=>{t.classList.toggle("active")})});
+//# sourceMappingURL=index.6ad2f3c1.js.map
